b7f4669f482e0e0b5bf5c704f899625097d22095
Guilherme Smith - Banco de Dados <guilherme.rodrigues@ixcsoft.com.br>
2025-05-12 18:33:50 -0300

    feat: login continuado Agora, tipo de autenticação é um campo

--- src/paginas/usuario/Login.jsx
+++ src/paginas/usuario/Login.jsx
@@ -3,47 +3,38 @@ import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { ToastContainer, toast } from 'react-toastify';
 
-// Componentes
-import BotaoAcao from '@componentes/BotaoAcao';
-
 const Login = () => {
-
-    const [authType, setAuthType] = useState('usuario');
-
-    const [endpoint, setEndpoint] = useState(authType);
-
-    const handleAuthType = (type) => {
-        setAuthType(type);
-        console.log(`Tipo de login: ${type}`);
-    }
-
-    useEffect(() => {
-        setEndpoint(authType);
-    },[authType]);
 
     const { register, handleSubmit } = useForm();
 
     const onSubmit = async (data) => {
         console.log('Dados enviados: ', data);
+
+        // Tratar login
     }
 
     return (
         <article className='container'>
             <h2 className='text-center'>Entrar</h2>
             <form onSubmit={handleSubmit(onSubmit)} className='form container alert alert-dark position-relative'>
-                <div className='d-flex justify-content-center gap-2'>
-                    <BotaoAcao label='Usuário' className='btn-dark' onClick={() => handleAuthType('usuario')} disabled={authType === 'usuario'} />
-                    <BotaoAcao label='Funcionário' className='btn-dark' onClick={() => handleAuthType('funcionario')} disabled={authType === 'funcionario'}/>
-                </div>
                 <div className='mb-3'>
                     <label htmlFor="email" className='form-label'>E-mail</label>
                     <input id='email' className='form-control' type="email" {...register("email")} placeholder='Digite seu e-mail' />
-                    <span className='text-muted'>Nós nunca compartilharemos seu e-mail com ninguém</span>
+                    <sub className='text-muted'>Nós nunca compartilharemos seu e-mail com ninguém</sub>
                 </div>
                 <div className='mb-3'>
                     <label htmlFor="senha" className='form-label'>Senha</label>
                     <input id='senha' className='form-control' type="password" {...register("senha")} placeholder='Digite sua senha' />
-                    <span className='text-muted'></span>
+                </div>
+                <div className='mb-3'>
+                    <div className='form-check'>
+                        <input type="radio" className='form-check-input' id='authTypeUsuario' value='usuario' name='authType' />
+                        <label htmlFor="authTypeUsuario" className='form-check-label'>Usuário</label>
+                    </div>
+                    <div className='form-check'>
+                        <input type="radio" className='form-check-input' id='authTypeFuncionario' value='funcionario' name='authType' />
+                        <label htmlFor="authTypeFuncionario" className='form-check-label'>Funcionário</label>
+                    </div>
                 </div>
                 <button type='submit' className='btn btn-success'>Entrar</button>
             </form>
