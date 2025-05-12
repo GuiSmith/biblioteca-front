aecf5c73aac1f65862e7bf2a7ffcb019e1c4b2f2
Guilherme Smith - Banco de Dados <guilherme.rodrigues@ixcsoft.com.br>
2025-05-12 12:49:25 -0300

    feat: LivroView parcialmente finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import API from '@servicos/API';
 function App() {
 
   // Temporário, até criar tela e funções de login
-  API.definirToken('62c6f125a1d127ca2efce7cac77a2a35aeaaef7e09a8b12782221a9e503873b205f88bf75a624fa1653243a1101a39d0b614992c5882e0a9ec881bdc23e54f62');
+  API.definirToken('1b1d0428eec47d23b851d589f25db5f9d8b4b99e8e631fc8780dbfa4357a824a339a73b32e7c7571bfe0519bf0bbdde20722abb1b44ad635cfad5624a52b6279');
 
   return (
     <section className = 'app-container'>
@@ -24,7 +24,6 @@ function App() {
           {rotas.map(({ path, element }, index) => (
             <Route key={index} path={path} element={element} />
           ))}
-          }
           </Routes>
         </div>
       </Router>
--- src/paginas/livro/LivroView.jsx
+++ src/paginas/livro/LivroView.jsx
@@ -5,16 +5,19 @@ import { useParams } from 'react-router-dom';
 
 //Serviços
 import API from '@servicos/API';
-import { listarAutores } from '@servicos/livro_autor';
+import { listarAutores } from '@servicos/livroAutor';
 
 // Componentes
 import BotaoAcao from '@componentes/BotaoAcao';
+
+// Utilitários
+import { isDate, formatDate } from '@utils/formatadores';
 
 const LivroView = () => {
 
 	const { id } = useParams();
 
-	if(!id){
+	if (!id) {
 		toast('ID inválido ou não informado!');
 	}
 
@@ -24,20 +27,20 @@ const LivroView = () => {
 
 	const apiOptions = API.apiOptions('GET');
 
-	const [livro,setLivro] = useState(null);
+	const [livro, setLivro] = useState(null);
 
-	const [exemplares,setExemplares] = useState([]);
+	const [exemplares, setExemplares] = useState([]);
 
-	const [autores,setAutores] = useState([]);
+	const [autores, setAutores] = useState([]);
 
 	useEffect(() => {
-		fetch(completeUrl,apiOptions)
+		fetch(completeUrl, apiOptions)
 			.then(async response => {
 				const data = await response.json();
 
-				if(response.status !== 200){
+				if (response.status !== 200) {
 					toast(data.mensagem);
-				}else{
+				} else {
 					setLivro((prev) => ({
 						...prev,
 						...data.livro
@@ -46,10 +49,10 @@ const LivroView = () => {
 				}
 
 			})
-	},[]);
+	}, []);
 
 	useEffect(() => {
-		if(livro){
+		if (livro) {
 			listarAutores(livro.id)
 				.then(autoresLista => setAutores(autoresLista))
 				.catch(error => {
@@ -58,37 +61,80 @@ const LivroView = () => {
 				})
 		}
 
-	},[livro]);
+	}, [livro]);
 
-	useEffect(() => {
-		if(autores){
-			// console.log(autores);
+	const renderTable = () => {
+
+		const dados = {
+			id: 'ID',
+			condicao_fisica: 'Condição Física',
+			edicao: 'Edição',
+			ano: 'Ano',
+			data_aquisicao: 'Data de aquisição',
+			situacao: 'Situação',
+			observacoes: 'Observações',
+		};
+
+		if (exemplares.length == 0) {
+			return (
+				<p className='text-muted'>Nenhum exemplar cadastrado</p>
+			);
 		}
-	},[autores]);
+
+		return (
+			<table className='table table-stripped table-dark'>
+				<thead>
+					<tr>
+						{Object.entries(dados).map(([chave, valor]) => <th key={chave}>{valor}</th>)}
+					</tr>
+				</thead>
+				<tbody>
+					{exemplares.map(exemplar => (
+						<tr key={exemplar.id}>
+							{Object.keys(dados).map((chave, index) => (
+								<td key={index}>{isDate(exemplar[chave]) ? formatDate(exemplar[chave]) : exemplar[chave]}</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
+		)
+
+	};
 
 	return (
-		<article className='alert alert-primary p-3 container'>
-			{!livro ? (
-				<p>Carregando...</p>
-			) : (
-				<>
-					<h2>{livro.titulo || 'Titulo'}</h2>
-					<p>Sinopse: {livro.sinopse || 'sinopse'}</p>
-					<p>Enredo: {livro.enredo}</p>
-					<p>
-						Autores: {
-							autores.length > 0
-							? autores.reduce((acc, autor, index) => acc + (index > 0 ? ', ' : '') + autor.nome, '')
-							: ''
-						}
-					</p>
-					<hr/>
-					<BotaoAcao label='Fazer algo' className='btn-primary' onClick = {() => console.table(livro)} />
-				</>
-			)}
+		<article className='container'>
+			<div className='alert alert-primary text-dark p-3'>
+				{!livro ? (
+					<p>Carregando...</p>
+				) : (
+					<>
+						<h2>{livro.titulo || 'Titulo'}</h2>
+						<p>Sinopse: {livro.sinopse || 'sinopse'}</p>
+						<p>Enredo: {livro.enredo}</p>
+						<p>
+							Autores: {
+								autores.length > 0
+									? autores.reduce((acc, autor, index) => acc + (index > 0 ? ', ' : '') + autor.nome, '')
+									: ''
+							}
+						</p>
+						<hr />
+						<BotaoAcao label='Fazer algo' className='btn-primary' onClick={() => console.table(livro)} />
+					</>
+				)}
+			</div>
+			<div>
+				<h2 className='text-center'>Exemplares</h2>
+				{exemplares.length == 0
+					? <p className='text-muted'>Nenhum exemplar cadastrado</p> 
+					: renderTable()
+				}
+			</div>
+			<ToastContainer position='bottom-right' />
 		</article>
 	)
 };
 
-export default LivroView; 
+export default LivroView;
 
