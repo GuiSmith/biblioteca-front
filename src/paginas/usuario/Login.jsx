eefb68948387b620667774d78f8a7f0fc823c8d1
GuiSmith <guilhermessmith2014@gmail.com>
2025-05-14 19:48:09 -0300

    feat: Login iniciado

--- src/paginas/usuario/Login.jsx
+++ src/paginas/usuario/Login.jsx
@@ -19,20 +19,20 @@ const Login = () => {
             <form onSubmit={handleSubmit(onSubmit)} className='form container alert alert-dark position-relative'>
                 <div className='mb-3'>
                     <label htmlFor="email" className='form-label'>E-mail</label>
-                    <input id='email' className='form-control' type="email" {...register("email")} placeholder='Digite seu e-mail' />
+                    <input id='email' className='form-control' type="email" placeholder='Digite seu e-mail' {...register("email")} />
                     <sub className='text-muted'>Nós nunca compartilharemos seu e-mail com ninguém</sub>
                 </div>
                 <div className='mb-3'>
                     <label htmlFor="senha" className='form-label'>Senha</label>
-                    <input id='senha' className='form-control' type="password" {...register("senha")} placeholder='Digite sua senha' />
+                    <input id='senha' className='form-control' type="password" placeholder='Digite sua senha' {...register("senha")} />
                 </div>
                 <div className='mb-3'>
                     <div className='form-check'>
-                        <input type="radio" className='form-check-input' id='authTypeUsuario' value='usuario' name='authType' />
+                        <input type="radio" className='form-check-input' id='authTypeUsuario' value='usuario' name='authType' {...register("authType")} />
                         <label htmlFor="authTypeUsuario" className='form-check-label'>Usuário</label>
                     </div>
                     <div className='form-check'>
-                        <input type="radio" className='form-check-input' id='authTypeFuncionario' value='funcionario' name='authType' />
+                        <input type="radio" className='form-check-input' id='authTypeFuncionario' value='funcionario' name='authType' {...register("authType")} />
                         <label htmlFor="authTypeFuncionario" className='form-check-label'>Funcionário</label>
                     </div>
                 </div>
