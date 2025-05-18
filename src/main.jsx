6bba370743debe910f88c7434dba4140333ce22e
Guilherme Smith - Banco de Dados <guilherme.rodrigues@ixcsoft.com.br>
2025-05-18 08:55:38 -0300

    feat: bootstrap icons added

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; // ✅ Import here
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Optional
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
