97201c3f21c9c595d583ea32ed2d1bac0ded4eb3
Guilherme Smith - Banco de Dados <guilherme.rodrigues@ixcsoft.com.br>
2025-05-18 19:32:01 -0300

    feat: rotas alteradas, novamente

--- src/rotas.jsx
+++ src/rotas.jsx
@@ -3,9 +3,9 @@ import { Login } from '@paginas/usuario';
 
 const rotas = [
 	{ auth: false, path: '/livros', element: <LivroLista /> },
-	{ auth: false, path: '/livro/:id', element: <LivroView/> },
-	{ auth: true, path: '/livro/:id', element: <LivroRegistro /> },
-	{ auth: true, path: '/livro/novo', element: <LivroRegistro /> },
+	{ auth: false, path: '/livro/view/:id', element: <LivroView/> },
+	{ auth: true, path: '/livro/form/:id', element: <LivroRegistro /> },
+	{ auth: true, path: '/livro/form/novo', element: <LivroRegistro /> },
 	{ auth: false, path: '/login', element: <Login />}
 ];
 
