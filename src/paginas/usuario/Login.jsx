e26f5f46e82ebd9d5ef048d58a099ff478cd4d76
Guilherme Smith - Banco de Dados <guilherme.rodrigues@ixcsoft.com.br>
2025-05-18 08:57:24 -0300

    feat: login finalizado

--- src/paginas/usuario/Login.jsx
+++ src/paginas/usuario/Login.jsx
@@ -1,36 +1,72 @@
 // Bibliotecas
-import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { ToastContainer, toast } from 'react-toastify';
+
+// Serviços
+import API from '@servicos/API';
 
 const Login = () => {
 
     const { register, handleSubmit } = useForm();
 
+    const navigate = useNavigate();
+
     const onSubmit = async (data) => {
         console.log('Dados enviados: ', data);
 
-        // Tratar login
+        try {
+            const authTypeOptions = ['usuario', 'funcionario'];
+            const authType = authTypeOptions.find(type => type === data.authType);
+            if (!authType) {
+                toast.error('Tipo de autenticação inválido');
+                return;
+            }
+
+            const endpoint = `${authType}/login`;
+            const completeUrl = `${API.apiUrl}/${endpoint}`;
+
+            const response = await fetch(completeUrl,API.apiOptions('POST',data));
+            const responseData = await response.json();
+
+            if(response.status == 200){
+                API.definirToken(responseData.token);
+                toast.success('Login realizado com sucesso');
+                navigate('/livros');
+            }
+
+            toast.warning(responseData.mensagem);
+
+        } catch (error) {
+            console.error('Erro ao fazer login: ', error);
+            toast.error('Erro ao fazer login, tente novamente mais tarde');
+        }
+
     }
 
     return (
         <article className='container'>
             <h2 className='text-center'>Entrar</h2>
             <form onSubmit={handleSubmit(onSubmit)} className='form container alert alert-dark position-relative'>
+                {/* E-mail */}
                 <div className='mb-3'>
                     <label htmlFor="email" className='form-label'>E-mail</label>
                     <input id='email' className='form-control' type="email" placeholder='Digite seu e-mail' {...register("email")} />
                     <sub className='text-muted'>Nós nunca compartilharemos seu e-mail com ninguém</sub>
                 </div>
+                {/* Senha */}
                 <div className='mb-3'>
                     <label htmlFor="senha" className='form-label'>Senha</label>
                     <input id='senha' className='form-control' type="password" placeholder='Digite sua senha' {...register("senha")} />
                 </div>
+                {/* Tipo de autenticação */}
                 <div className='mb-3'>
+                    {/* Usuário */}
                     <div className='form-check'>
                         <input type="radio" className='form-check-input' id='authTypeUsuario' value='usuario' name='authType' {...register("authType")} />
                         <label htmlFor="authTypeUsuario" className='form-check-label'>Usuário</label>
                     </div>
+                    {/* Funcionário */}
                     <div className='form-check'>
                         <input type="radio" className='form-check-input' id='authTypeFuncionario' value='funcionario' name='authType' {...register("authType")} />
                         <label htmlFor="authTypeFuncionario" className='form-check-label'>Funcionário</label>
