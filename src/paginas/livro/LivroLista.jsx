67fc3a3cb10eecb98a411ef476f4d2ae021e3b0f
Guilherme Smith - Banco de Dados <guilherme.rodrigues@ixcsoft.com.br>
2025-05-23 18:35:22 -0300

    feat: categoria e ativo agora aparecem

--- src/paginas/livro/LivroLista.jsx
+++ src/paginas/livro/LivroLista.jsx
@@ -1,20 +1,44 @@
 import { ToastContainer, toast } from 'react-toastify';
 import { useEffect, useState } from 'react';
 
-import api from '@servicos/API';
+import API from '@servicos/API';
 import Cartao from '@componentes/Cartao';
 import BotaoLink from '@componentes/BotaoLink';
 import BotaoAcao from '@componentes/BotaoAcao';
+import { useFetcher } from 'react-router-dom';
 
 const LivroLista = () => {
 
     const endpoint = 'livro';
-    const completeUrl = `${api.apiUrl}/livro`;
-    const apiOptions = api.apiOptions('GET');
+    const completeUrl = `${API.apiUrl}/livro`;
+    const apiOptions = API.apiOptions('GET');
 
     const [cartoes, setCartoes] = useState('');
+    const [categorias, setCategorias] = useState([]);
 
+    // Categorias
     useEffect(() => {
+        API.listar('categoria')
+            .then(responseCategoria => {
+                if (responseCategoria.ok) {
+                    setCategorias(responseCategoria.array);
+                    return;
+                }
+                if (responseCategoria.error) {
+                    toast.error('Erro ao listar categorias');
+                    return;
+                }
+                if (responseCategoria.ok === false && responseCategoria.error === false) {
+                    toast.warning(responseCategoria.mensagem);
+                    return;
+                }
+            })
+            .catch(error => toast.error('Erro ao listar categorias'));
+    }, []);
+
+    // Livros
+    useEffect(() => {
+
         fetch(completeUrl, apiOptions)
             .then(async response => {
                 const data = await response.json();
@@ -24,23 +48,31 @@ const LivroLista = () => {
                     return;
                 }
 
-                console.log(data);
-
                 setCartoes(
                     <div className='d-flex flex-wrap justify-content-center gap-3'>
                         {data.map(async livro => {
+
+                            if (categorias.length > 0) {
+                                const categoria = categorias.find(categoria => livro.id_categoria = categoria.id);
+
+                                livro.categoria = categoria.nome ?? '';
+                            }else{
+                                livro.categoria = 'Carregando...';
+                            }
+
                             const botoes = [
-                                <BotaoLink to={`/livro/view/${livro.id}`} label = 'Detalhes' className = 'btn-primary' />,
-                                <BotaoLink label = 'Emprestar' className = 'btn-success' />
+                                <BotaoLink to={`/livro/view/${livro.id}`} label='Detalhes' className='btn-primary' />,
+                                <BotaoLink label='Emprestar' className='btn-success' />
                             ];
 
                             return (
                                 <div key={livro.id}>
-                                    <Cartao img={{ src: livro.foto, alt: `Imagem: ${livro.titulo}` }} titulo={livro.titulo} botoes={botoes} className = 'mb-3'>
-                                        {/* <p>{`Autores: ${nomeAutores}`}</p> */}
-                                        {/* <p>{`Sinopse: ${livro.sinopse}`}</p> */}
-                                        {/* <p>5 exemplares</p> */}
-                                        <p>{livro.ativo ? 'Ativo' : 'Inativo'}</p>
+                                    <Cartao img={{ src: livro.foto, alt: `Imagem: ${livro.titulo}` }} titulo={livro.titulo} botoes={botoes} className='mb-3'>
+                                        <p className='text-muted'>
+                                            {livro.ativo ? <span className='text-dark'>Ativo</span> : <span className='text-muted'>Inativo</span>}
+                                            <br />
+                                            {livro.categoria}
+                                        </p>
                                     </Cartao>
                                 </div>
                             )
@@ -52,7 +84,7 @@ const LivroLista = () => {
                 toast.error('Erro ao buscar dados');
                 console.error('Error fetching data:', error);
             });
-    }, []);
+    }, [categorias]);
 
     return (
         <article>
