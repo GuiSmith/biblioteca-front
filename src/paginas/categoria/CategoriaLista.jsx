2f14996e27dfaeca2cdf5bf24313a142a8391d2e
Guilherme Smith - Banco de Dados <guilherme.rodrigues@ixcsoft.com.br>
2025-05-28 19:32:05 -0300

    feat: categoria lista agora é tabela

--- src/paginas/categoria/CategoriaLista.jsx
+++ src/paginas/categoria/CategoriaLista.jsx
@@ -1,6 +1,7 @@
 // Bibliotecas
 import { ToastContainer, toast } from "react-toastify";
 import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 //Serviços
 import API from '@servicos/API';
@@ -11,6 +12,13 @@ import BotaoLink from '@componentes/BotaoLink';
 const CategoriaLista = () => {
 
     const [categorias, setCategorias] = useState([]);
+
+    const colunas = {
+        id: 'ID',
+        nome: 'Nome',
+    };
+
+    const navigate = useNavigate();
 
     useEffect(() => {
 
@@ -35,28 +43,31 @@ const CategoriaLista = () => {
     }, []);
 
     return (
-        <section className="container-fluid">
+        <section className="container">
             <h1 className="text-center">Categorias</h1>
             <div className="d-flex flex-wrap justify-content-start gap-2 mb-3 mt-3">
                 <BotaoLink label='Novo' to='/categoria/form/novo' className='btn-primary' />
             </div>
-            <article className="d-flex flex-wrap justify-content-start gap-2">
-                {categorias && (
-                    categorias.map(categoria => (
-                        <div key={categoria.id} id={categoria.id} className="card">
-                            <div className="card-body">
-                                <h5 className='card-title'>{categoria.nome}</h5>
-                                <p className="card-text">
-                                    {categoria.descricao ? (categoria.descricao.slice(0, 37) + '...') : ''}
-                                </p>
-                                <div className="d-flex flex-wrap justify-content-start gap-2">
-                                    <BotaoLink label='Detalhes' to={`/categoria/view/${categoria.id}`} className='btn-dark' />
-                                    <BotaoLink label='Livros' to={`/categoria/view/${categoria.id}#livros`} className='btn-success' />
-                                </div>
-                            </div>
-                        </div>
-                    ))
-                )}
+            <article className="table-container">
+                {!categorias
+                    ? <p className="text-center">Carregando...</p>
+                    : (
+                        <table className="table table-stripped table-dark">
+                            <thead>
+                                <tr>
+                                    {Object.values(colunas).map((coluna,index) => <th key={index}>{coluna}</th>)}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {categorias.map(categoria => (
+                                    <tr key={categoria.id} className="cursor-pointer" onClick={() => navigate(`/categoria/view/${categoria.id}`)} >
+                                        {Object.keys(colunas).map((coluna,index) => <td key={index}>{categoria[coluna]}</td>)}
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    )
+                }
             </article>
             <ToastContainer position="bottom-right" />
         </section>
